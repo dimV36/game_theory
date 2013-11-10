ca873e622f49a367d5f5f17bfa602c47b9bd085f
Dmitry Voronin <carriingfate92@yandex.ru>
2013-11-10 13:44:19 +0400

    clean

--- game_theory/entity/__init__.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'dimv36'
-__all__ = ["AlphaBeta", "BiMatrix", "SrpGamersProbabilityDistribution", "GameStrategy"]
-from .BiMatrix import BiMatrix
-from .AlphaBeta import AlphaBeta
-from .GameStrategy import GameStrategy
-from .SrpGamersProbabilityDistribution import SrpGamersProbabilityDistribution
